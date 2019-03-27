9d4c66f1eda7565ab1942f20583678a1d14907b5
visualstudio <visualstudio@122-PC34>
2019-03-27 12:00:32 -0400

    commit 11:50

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +19,46 @@ namespace ConsoleApp2
 
         static void Download()
         {
-            Task.Run(() => {
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace LearningProperties
+    {
+        class Player
+        {
+            public bool isAlive = true;
+            int health = 100;
+
+            public void Hit()
+            {
+                Random r = new Random();
+                health -= r.Next(5, 50);
+            }
+
+            public void CheckDeath()
+            {
+                if (health <= 0)
+                    isAlive = false;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Player player = new Player();
+                for (int i = 0; i < 20; i++)
+                {
+                    player.Hit();
+                    player.CheckDeath();
+                    Console.WriteLine("Is player alive: " + player.isAlive);
+                }
+            }
+        }
+    }
+    Task.Run(() => {
 
                 Thread.Sleep(3000);
                 Console.WriteLine("Download  Complete");
